5fc86e245d6e1439b4312220cd654c9e32eafaad
LukaAlhonen <luka.alhonen@protonmail.com>
2024-01-28 17:41:46 +0200

    Refactored to xcode project

---
+++ Compiler/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Compiler
+//
+//  Created by Luka Alhonen on 28.1.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
